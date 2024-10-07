095038be824f8b7ea810b82b059ca3284b5b7a90
emmanuelSjamesB <emmanuel.james@gmail.com>
2024-10-07 00:39:34 -0500

    magen

--- src/components/NavCmd.tsx
+++ src/components/NavCmd.tsx
@@ -31,7 +31,7 @@ const Navbar: React.FC = () => {
   }, []);
 
   return (
-    <nav className={`fixed top-0 w-screen transition-opacity duration-150 ${isVisible ? 'bg-black p-4 border-b-2 z-30 border-dashed border-green-500 opacity-100' : 'bg-gradient-to-r z-40 from-gray-900 via-blue-800 to-blue-500 p-4 opacity-100'}`}>
+    <nav className={`fixed top-0 w-screen transition-opacity duration-150 ${isVisible ? 'bg-black p-4 border-b-2 z-40 border-dashed border-green-500 opacity-100' : 'bg-gradient-to-r z-40 from-gray-900 via-blue-800 to-blue-500 p-4 opacity-100'}`}>
       <div className="container mx-auto flex justify-between items-center">
         <div className="flex items-center">
           <LaptopIcon />
