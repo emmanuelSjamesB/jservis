0ddd70eb584ded06080f5125385988912e19299e
emmanuelSjamesB <emmanuel.james@gmail.com>
2024-10-07 23:41:46 -0500

    magen

--- src/components/VideoHeader.tsx
+++ src/components/VideoHeader.tsx
@@ -4,7 +4,7 @@ import video from '../assets/video.mp4';
 const VideoHeader: React.FC = () => {
   return (
     <header className="relative flex items-center justify-center h-screen overflow-hidden bg-black w-full">
-      <div className="relative  p-5 text-3xl font-bold mb-8 text-center text-black bg-purple-300 bg-opacity-50 rounded-xl">
+      <div className="relative z-10 p-5 text-3xl font-bold mb-8 text-center text-black bg-purple-300 bg-opacity-50 rounded-xl">
         Reparación y optimización
       </div>
       <video
